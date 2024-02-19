e49f669b7557741cd0b525f4a33d6959013e5db6
eleanor-rf <eleanorfrank@outlook.com>
2024-02-19 11:16:58 +0000

    complete get topics feature

---
+++ __tests__/app.test.js
@@ -0,0 +1,24 @@
+const app = require("../app.js");
+const request = require("supertest");
+const db = require("../db/connection.js");
+const seed = require("../db/seeds/seed.js");
+const testData = require("../db/data/test-data");
+
+beforeEach(() => seed(testData));
+afterAll(() => db.end());
+
+describe("GET /api/topics", () => {
+  it("should get all topics", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then((response) => {
+        const topics = response.body.topics;
+        expect(topics.length).toBe(3);
+        topics.forEach((entry) => {
+          expect(typeof entry.slug).toBe("string");
+          expect(typeof entry.description).toBe("string");
+        });
+      });
+  });
+});
